261c51a43cc7d66e7e4929749ce4e5490591fcc5
Arrudonimous <arrudadiego45@gmail.com>
2023-02-28 13:09:06 -0300

    Feat: changing login

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -12,20 +12,20 @@ import Sol from '../../public/SOL.png';
 
 export default function Home() {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
   const [isSearching, setIsSearching] = useState(false)
   const [number1, setNumber1 ] = useState(0)
   const [number2, setNumber2 ] = useState(0)
 
-  useEffect(()=>{
-    setIsLoading(true)
-    const token = getCookie('token');
-    if(!token){
-      setIsLoading(false)
-      return router.push('/')
-    }
-    setIsLoading(false)
-  },[])
+  // useEffect(()=>{
+  //   setIsLoading(true)
+  //   const token = getCookie('token');
+  //   if(!token){
+  //     setIsLoading(false)
+  //     return router.push('/')
+  //   }
+  //   setIsLoading(false)
+  // },[])
 
   function getRandomInt(min: number, max: number) {
     return ((Math.random() * (max - min + 1)) + min).toFixed(2);
