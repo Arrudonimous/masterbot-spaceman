d9237a1cfd5cf6d3f88e9d3f5cd6b557f0d9d8cc
Arrudonimous <arrudadiego45@gmail.com>
2023-03-02 16:45:48 -0300

    Fix

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -69,8 +69,8 @@ export default function Home() {
         <main className="flex flex-col">
         <NavBar />
 
-        <div className='w-full flex justify-center items-center flex-col mb-12'>
-          <Image src={Logo} alt="logo" className='w-80'/>
+        <div className='w-full flex justify-center items-center flex-col mb-3'>
+          <Image src={Logo} alt="logo" className='md:w-72 w-40'/>
         </div>
 
         <div className='md:px-20'>
@@ -78,7 +78,7 @@ export default function Home() {
             <h1 className='flex text-white mb-4 text-2xl'>Clique aqui para gerar sinais</h1>
             {isSearching ? (
               <div>
-                <Image src={Sol} width={200} height={200} className="w-20 h-20 animate-spin duration-1000" alt='helice'/>
+                <Image src={Sol} width={200} height={200} className="w-[55px] h-[55px] animate-spin duration-1000" alt='helice'/>
               </div>
             ):(
               <button 
@@ -86,31 +86,31 @@ export default function Home() {
                   getValues();
                 }}
                 id='neonButton'
-                className='bg-transparent px-24 py-6 text-lg rounded-xl text-white drop-shadow-md border-2 border-pink-500 hover:border-pink-300 ease-in-out transition-colors active:border-blue-600'>
+                className='bg-transparent px-24 py-3 text-lg rounded-xl text-white drop-shadow-md border-2 border-pink-500 hover:border-pink-300 ease-in-out transition-colors active:border-blue-600'>
                 DECOLAR
               </button>
             )}
 
-            <div className='flex flex-col md:flex-row mt-4 text-black mb-20 md:mb-32 justify-center items-center md:justify-around w-full gap-5 md:gap-0'>
-              <div className='w-3/4 md:w-1/3 bg-background-botao bg-contain h-60 bg-center bg-no-repeat rounded-3xl px-3 py-16 flex flex-col  drop-shadow-lg items-center'>
+            <div className='flex flex-col md:flex-row mt-4 text-black justify-center items-center md:justify-around w-full gap-5 md:gap-0 mb-32 md:mb-0'>
+              <div className='w-3/4 md:w-1/3 bg-background-botao bg-contain h-48 md:h-60 bg-center bg-no-repeat rounded-3xl px-3 py-16 flex flex-col  drop-shadow-lg items-center'>
                 <h1 className='text-xl'>Primeiro Valor</h1>
                 <div className='w-full h-full flex justify-center items-center'>
                   <AnimatedNumber 
                     value={number1}
-                    className='text-5xl md:text-7xl'
+                    className='text-4xl md:text-7xl'
                     formatValue={(n: number) => n.toFixed(2)}
                     duration={800}
                   />
                 </div>
               </div>
 
-              <div className='w-3/4 md:w-1/3 bg-background-botao bg-contain h-60 bg-center bg-no-repeat rounded-3xl px-3 py-16 flex flex-col  drop-shadow-lg items-center'>
+              <div className='w-3/4 md:w-1/3 bg-background-botao bg-contain h-48 md:h-60 bg-center bg-no-repeat rounded-3xl px-3 py-16 flex flex-col  drop-shadow-lg items-center'>
                 <h1 className='text-xl'>Segundo Valor</h1>
                 <div className='w-full h-full flex justify-center items-center'>
                   
                   <AnimatedNumber 
                     value={number2}
-                    className='text-5xl md:text-7xl'
+                    className='text-4xl md:text-7xl'
                     formatValue={(n: number) => n.toFixed(2)}
                     duration={800}
                   />
